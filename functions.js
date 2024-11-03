7db38624b9bfeb1ee408802b3644c2f5b002310e
ousef777 <yousef.alfeeli@gmail.com>
2024-11-03 12:15:14 +0300

    Solution

--- functions.js
+++ functions.js
@@ -1,10 +1,14 @@
+const { fi } = require("@faker-js/faker");
+
 /**
  * Task 1:
  * Create a function named `printName`
  * - take a name as a parameter
  * - that just prints that name on the screen
  */
-
+function printName(name) {
+  console.log(name);
+}
 /**
  * Task 2:
  * Create a function named `printAge`
@@ -12,7 +16,9 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
-
+function printAge(birthYear) {
+  console.log(2024 - birthYear);
+}
 /**
  * Task 3:
  * Create a function named `printHello`
@@ -23,19 +29,29 @@
  * -- fr: it should print `Bonjour NAME`
  * -- tr: it should print `Merhaba NAME`
  */
-
+function printHello(name, language) {
+  let first = "Hello ";
+  if (language == "es") first = "Hola ";
+  else if (language == "fr") first = "Bonjour ";
+  else if (language == "tr") first = "Merhaba ";
+  console.log(first + name);
+}
 /**
  * Task 4:
  * Create a function named `printMax`
  * - that takes 2 parameters as numbers
  * - should print out the bigger number
  */
+function printMax(a, b) {
+  console.log((a > b) ? a : b);
+}
 
+console.log(printMax(3, 2));
 // TODO: Uncomment these functions as you work on them
 // Make sure they're all uncommented before you push your solution!
 module.exports = {
-  // printName,
-  // printAge,
-  // printHello,
-  // printMax,
+  printName,
+  printAge,
+  printHello,
+  printMax,
 };
